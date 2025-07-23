0bf229514797de960e48b13b66a07aa2cd1cd37e
Angel Ochoa <angel8a@stanford.edu>
2025-07-22 17:08:26 -0700

    Updated Homepage to reflect relevant variants

--- apps/platform/src/pages/HomePage/HomePageSuggestions.tsx
+++ apps/platform/src/pages/HomePage/HomePageSuggestions.tsx
@@ -8,7 +8,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Chip, Grid } from "@mui/material";
 import { styled } from "@mui/styles";
-import { Link, useSearchState } from "ui";
+import { Link } from "ui";
 
 const StyledChip = styled(Chip)(({ theme }) => ({
   border: 1,
@@ -29,12 +29,18 @@ interface SearchSuggestion {
   name: string;
 }
 
+// Static Array of Suggestions Related to Lab Papers
+const staticVariants: SearchSuggestion[] = [
+  { id: "5_173860848_G_A", name: "rs875741" },
+  { id: "4_142405561_T_G", name: "rs7696969" },
+  { id: "1_99580690_T_G", name: "rs6702619" },
+];
+
 function HomePageSuggestions() {
-  const { searchSuggestions } = useSearchState() as { searchSuggestions: SearchSuggestion[] };
   return (
     <Grid container justifyContent="center" gap={1.5} sx={{ mt: 4 }}>
-      {searchSuggestions.map(suggestion => (
-        <Link asyncTooltip to={`/variant/${suggestion.id}`}>
+      {staticVariants.map(suggestion => (
+        <Link asyncTooltip to={`/variant/${suggestion.id}`} key={suggestion.id}>
           <StyledChip
             sx={{ pl: 1, borderRadius: 2 }}
             icon={<FontAwesomeIcon icon={faMapPin} />}
